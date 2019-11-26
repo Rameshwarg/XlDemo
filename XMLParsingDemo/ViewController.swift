e5b29d702ce1847dc92568f2d60a3d5b6710b8fc
Sandeep Kakde <sandeepkakde@discovery.bank>
2019-11-26 17:47:01 +0530

    Initial Commit

---
+++ XMLParsingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XMLParsingDemo
+//
+//  Created by Sandeep Kakde on 2019/11/26.
+//  Copyright © 2019 Sandeep Kakde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
